2a1855c60e81f1803155fc2cb4a6026f8a6f0688
Comevius <comevius@gmail.com>
2018-02-07 00:35:15 +0100

    Emacs: remove Marmalade from package-archives

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,6 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (package-initialize)
